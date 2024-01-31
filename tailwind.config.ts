27470a1cf143246335b5b34f21d4c3cf9317afbc
Parth Doshi <parthdoshi480@gmail.com>
2024-01-31 10:49:45 +0530

    Basic Config Files added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,31 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
     extend: {
+      fontFamily: {
+        Poppins : ["var(--font-Poppins)"], 
+        Josefin : ["var(--font-Josefin)"], 
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens:{
+        'xs': '320px',
+        'sm': '480px',
+        'md': '640px',
+        'lg': '768px',
+        'xl': '1024px',
+        '2xl': '1280px',
+        '3xl': '1536px',
+        '4xl': '1920px',
+        '5xl': '2560px',
+        '6xl': '3840px',
+        '7xl': '5120px',
+        '8xl': '7680px',
       },
     },
   },
